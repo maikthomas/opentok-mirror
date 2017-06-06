5a410689dd12530ea3bb0e70372924c06e9dc218
Mike Thomas <mthomas@tokbox.com>
2017-06-06 14:45:02 +0200

    Fix webpack devtool config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   context: path.join(__dirname, 'src'),
-  devtool: debug ? 'inline-sourcemap' : null,
+  devtool: debug ? 'inline-sourcemap' : false,
   entry: './js/App.js',
   module: {
     loaders: [
